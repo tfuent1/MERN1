f8532ee2804a6101e05e21f03baa44d4e84ebb37
Tommy <tfuentes415@gmail.com>
2024-05-07 22:41:30 -0500

    New MERN Project

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+    'http://localhost:3000'
+    /* 
+    add any URLs for websites allowed to access our REST API
+    'https://krkimball.com'
+    'https://www.krkimball.com
+    */
+]
+
+module.exports = allowedOrigins
